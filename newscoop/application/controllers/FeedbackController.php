571d1f35bd66d45cc535bc8f7fc08b41b04f6b00
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2011-12-12 17:54:38 +0100

    WOBS-691: Send omnibox feedback to dienstpult@tageswoche.ch

--- newscoop/application/controllers/FeedbackController.php
+++ newscoop/application/controllers/FeedbackController.php
@@ -151,12 +151,12 @@ class FeedbackController extends Zend_Controller_Action
         $fromEmail = $user->getEmail();
         
         $message = $values['message'];
-        $message = $message.'\nVon <a href="http://www.tageswoche.ch/user/profile/'.$user->getUsername().'">'.$user->getUsername().'</a> ('.$user->getRealName().')';
-        $message = $message.'\nGesendet von: <a href="'.$values['url'].'">'.$values['url'].'</a>';
+        $message = $message.'<br>Von <a href="http://www.tageswoche.ch/user/profile/'.$user->getUsername().'">'.$user->getUsername().'</a> ('.$user->getRealName().')';
+        $message = $message.'<br>Gesendet von: <a href="'.$values['url'].'">'.$values['url'].'</a>';
         
         $mail = new Zend_Mail('utf-8');
         $mail->setSubject('Leserfeedback: '.$values['subject']);
-        $mail->setBodyText($message);
+        //$mail->setBodyText($message);
         $mail->setBodyHtml($message);
         $mail->setFrom($fromEmail);
         $mail->addTo($toEmail);
